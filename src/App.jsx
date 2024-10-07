ce8a44080a2352fcda4b670f8da175123262a983
malhiya <malhiya.arnib17@gmail.com>
2024-10-07 19:12:59 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,70 @@ import './App.css';
 import {useState} from 'react';
 import Card from "./components/Card";
 import backgroundImg from "./images/background.png"
-
+// { answer:"Good morning", queston:"صباح الخي"}
 const App = () => {
   const [studyList, setStudyList] = useState([
-  { question:"Good morning", answer:"صباح الخير"},
-  { question:"Thank you", answer:"شكرًا لك"},
-  { question:"How are you?", answer:"كيف حالك"},
-  { question:"What is your name?", answer:"ما اسمك؟"},
-  { question:"God willing", answer:"إن شاء الله‎"},
-  { question:"Let's go", answer:"يللا"},
-  { question:"See you later", answer:"إلى اللقاء"},
-  { question:"You’re welcome", answer:"عفواً (Afwan)"},
-  { question:"Yes", answer:"نعم"},
-  { question:"No", answer:"لا"},
+  { answer:"Thank you", question:"شكرًا لك"},
+  { answer:"How are you?", question:"كيف حالك"},
+  { answer:"What is your name?", question:"ما اسمك؟"},
+  { answer:"God willing", question:"إن شاء الله‎"},
+  { answer:"Let's go", question:"يللا"},
+  { answer:"See you later", question:"إلى اللقاء"},
+  { answer:"You’re welcome", question:"عفواً"},
+  { answer:"Yes", question:"نعم"},
+  { answer:"No", question:"لا"},
+  { answer:"Good morning", queston:"صباح الخي"}
   ]);
 
   const [index, setIndex] = useState(0);
   const [isTurned, setisTurned] = useState(false);
+  const [guessInput, setGuessInput] = useState("");
+  const [inputClass, setInputClass] = useState('default-input');
+
 
 
   const handleCardClick = () => {
-    setIsTurned(!isTurned); // Toggle the flipped state
+    setisTurned(!isTurned); // Toggle the flipped state
   };
 
   const handleNextQuestion = () => {
     if (index === studyList.length - 1) {
       setIndex(0);
     } else {
-      setIndex(index + 1)
+      setIndex(index + 1);
     }
+    // reset card side to be on english first
     setisTurned(false);
+    setInputClass('default-input');
+    setGuessInput("");
   };
+
+  const handlePrevQuestion = () => {
+    if (index === 0) {
+      return;
+    } else {
+      setInputClass('default-input');
+      setGuessInput("");
+      setIndex(index - 1); 
+    }
+    checkIsTurned(false); 
+  };
+
+
+  const handleInputAnswer = (e) => {
+    setGuessInput(e.target.value);
+  };
+
+  const checkAnswer = () => {
+    const realAnswer = studyList[index].answer.toLowerCase();
+    const guess = guessInput.toLowerCase();
+   
+    if (realAnswer === guess) {
+      setInputClass('valid-input'); 
+    } else {
+      setInputClass('invalid-input'); 
+    }
+  }
 
   return (
 
@@ -49,9 +82,28 @@ const App = () => {
           answer={studyList[index].answer}
           isTurned={isTurned}
           setIsTurned={setisTurned}
+          onNext = {handleNextQuestion}
         />
       </div>
+
+      <div className="input-area"> 
+        <p>Type your answer here: </p>
+        <input 
+          type="text" 
+          className={inputClass} 
+          placeholder="Answer" 
+          value={guessInput} 
+          onChange={handleInputAnswer}>
+        </input>
+        <button id="submit-btn" onClick={checkAnswer}> Submit</button>
+      </div>
+      
+      <br></br>
+
+
+
       <div>
+        <button id="prev" onClick={handlePrevQuestion}> ← </button>
         <button id="next" onClick={handleNextQuestion}> → </button>
       </div>
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 const Card = (props) => {
-    const handleTurnCard = () => {
-        props.setIsTurned(!props.isTurned);
-    }
+const handleTurnCard = () => {
+    props.setIsTurned(!props.isTurned);
+}
 
 
 
-    return (
-        <div className={`card ${props.isTurned ? 'flipped' : ''}`} onClick={handleTurnCard}>
-            <div className="front">
-                <h3>{props.isTurned ? props.answer: props.question}</h3>
-            </div>
-            <div className="back">
-                <h2>{props.isTurned ? props.question: props.answer}</h2>
-            </div>
+return (
+    <div className={`card ${props.isTurned ? 'flipped' : ''}`} onClick={handleTurnCard}>
+        <div className="front">
+            <h3>{props.isTurned ? props.answer: props.question}</h3>
         </div>
+        <div className="back">
+            <h2>{props.isTurned ? props.question: props.answer}</h2>
+        </div>
+    </div>
 
-    );
+);
 }
 
 export default Card;
